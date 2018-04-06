0dd8cb26fc9854ec17ab2a9a30ee12f134d5cfcd
Kengo Nagashima <nshi1201@gmail.com>
2018-04-06 15:38:08 +0000

    version 1.0.0

---
+++ R/package.r
@@ -0,0 +1,12 @@
+#' Prediction Intervals for Random-Effects Meta-Analysis
+#'
+#' @author
+#' Kengo Nagashima, Hisashi Noma, and Toshi A. Furukawa
+#' @name pimeta-package
+#' @rdname pimeta-package
+#' @aliases pimeta-package
+#' @docType package
+#' @keywords package
+#' @useDynLib pimeta, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+NULL
